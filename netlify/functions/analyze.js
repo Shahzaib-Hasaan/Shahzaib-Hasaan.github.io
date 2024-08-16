185d318363ef6b89aece0942152225ebd1a5c679
Shahzaib Hassan <s22barin1m01005@iub.edu.pk>
2024-08-16 18:05:30 +0500

    Update analyze.js

--- netlify/functions/analyze.js
+++ netlify/functions/analyze.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.handler = async function(event, context) {
     const { url, strategy } = event.queryStringParameters;
-    const API_KEY = process.env.API_KEY;
+    const API_KEY = process.env.PAGE_SPEED_API; // Use the environment variable PAGE_SPEED_API
 
     const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${API_KEY}&strategy=${strategy}&category=performance&category=accessibility&category=best-practices&category=seo`;
 
